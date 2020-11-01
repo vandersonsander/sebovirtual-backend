6210aa03c0db51ae115bf883aa728598e2b9eb6b
Vanderson Sander <vanderson_sander@hotmail.com>
2020-10-31 23:37:46 -0300

    chore: add atributo id para o jwtResponseDto

--- src/main/java/br/com/pi/sebovirtual/repositories/CondicaoRepository.java
+++ src/main/java/br/com/pi/sebovirtual/repositories/CondicaoRepository.java
@@ -1,8 +1,11 @@
 package br.com.pi.sebovirtual.repositories;
 
+import java.util.Optional;
+
 import br.com.pi.sebovirtual.entities.Condicao;
 import br.com.pi.sebovirtual.resource.BaseRepository;
 
 public interface CondicaoRepository extends BaseRepository<Condicao> {
-
+	
+	Optional<Condicao> findOneByDescricao(String descricao);
 }
--- src/main/java/br/com/pi/sebovirtual/services/UsuarioService.java
+++ src/main/java/br/com/pi/sebovirtual/services/UsuarioService.java
@@ -20,7 +20,15 @@ public class UsuarioService extends BaseService<Usuario, UsuarioRepository> {
 	
 	@Override
 	public Usuario store(Usuario entity) {
-		System.out.println(entity);
-		return repository.save(entity);
+		System.out.println("store");
+		return super.store(entity);
 	}
+
+	@Override
+	public Usuario update(Integer id, Usuario entity) {
+		System.out.println("update");
+		return super.update(id, entity);
+	}
+	
+	
 }
