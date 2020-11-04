b8e2776d5617eb62b7202c77f87e37afd09fb8d9
Juliana Aquino <julianafsa@gmail.com>
2020-11-04 05:02:17 -0300

    Correção do nome da entidade, repositorio e serviço.

--- src/main/java/br/com/pi/sebovirtual/controllers/MotivoDevolucaoController.java
+++ src/main/java/br/com/pi/sebovirtual/controllers/MotivoDevolucaoController.java
@@ -3,14 +3,14 @@ package br.com.pi.sebovirtual.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.pi.sebovirtual.entities.Subcategoria;
-import br.com.pi.sebovirtual.repositories.SubcategoriaRepository;
+import br.com.pi.sebovirtual.entities.MotivoDevolucao;
+import br.com.pi.sebovirtual.repositories.MotivoDevolucaoRepository;
 import br.com.pi.sebovirtual.resource.BaseController;
-import br.com.pi.sebovirtual.services.SubcategoriaService;
+import br.com.pi.sebovirtual.services.MotivoDevolucaoService;
 
 @RestController
 @RequestMapping("motivo-devolucao")
 public class MotivoDevolucaoController extends BaseController
-	<Subcategoria, SubcategoriaRepository, SubcategoriaService> {
+	<MotivoDevolucao, MotivoDevolucaoRepository, MotivoDevolucaoService> {
 
 }
