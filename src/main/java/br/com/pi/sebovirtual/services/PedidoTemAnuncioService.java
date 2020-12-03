03ef8acac8338d46fbf48ca168060ddc4abc1264
Vanderson <vanderson_sander@hotmail.com>
2020-12-03 14:20:19 -0300

    Validacao Usuario n Compra de Si mesmo

--- src/main/java/br/com/pi/sebovirtual/services/PedidoTemAnuncioService.java
+++ src/main/java/br/com/pi/sebovirtual/services/PedidoTemAnuncioService.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.pi.sebovirtual.dto.ListaItensPedidoDTO;
 import br.com.pi.sebovirtual.entities.Pedido;
@@ -23,6 +25,15 @@ public class PedidoTemAnuncioService extends BaseService
 	private PedidoService pedidoService;
 	
 	public Pedido storeItems(ListaItensPedidoDTO lista) {
+		// Verifica se o id do comprador é igual ao do vendedor
+		Integer compradorId = lista.getPedido().getUsuario().getId();
+		Integer vendedorId = anuncioService
+				.getOne(lista.getItens().get(0).getAnuncio().getId()).getUsuario().getId();
+		
+		if (compradorId == vendedorId)
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+					"Você não pode comprar seus próprios produtos");
+		
 		Pedido pedido = pedidoService.store(lista.getPedido());
 		
 		List<PedidoTemAnuncio> newItens = new ArrayList<>();
